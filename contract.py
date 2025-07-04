1a7761d86be7c11e5f2fe33337a3ba857b6402a1
amaury <amaurydelille92500@gmail.com>
2025-07-04 18:18:23 +0200

    feat(diagram + tests): init

--- contract.py
+++ contract.py
@@ -8,31 +8,44 @@ GUESS_ERROR="You cannot submit a guess if you didn't join the game."
 GUESS_TWICE_ERROR="You cannot submit more than one guess."
 DEADLINE_ERROR="You can neither guess nor join a game after the deadline."
 
-class GuessSeedGame(sp.Contract):
-    def __init__(self, admin, entry_fee, round_duration, deadline):
-        self.data.admin = admin
-        self.data.entry_fee = entry_fee
-        self.data.round_duration = round_duration
-        self.data.players = sp.map()
-        self.data.deadline = deadline
+def main():
+    class GuessSeedGame(sp.Contract):
+        def __init__(self, admin, entry_fee, round_duration, deadline):
+            self.data.admin = admin
+            self.data.entry_fee = entry_fee
+            self.data.round_duration = round_duration
+            self.data.players = sp.map()
+            self.data.deadline = deadline
 
-    @sp.entry_point
-    def join_game(self):
-        assert self.data.deadline > sp.now, DEADLINE_ERROR
-        assert sp.amount == 1, PAYMENT_ERROR
-        assert not self.data.players.contains(sp.sender), JOIN_ERROR
-        
-        self.data.players[sp.sender] = sp.string("")
+        @sp.entry_point
+        def join_game(self):
+            assert self.data.deadline > sp.now, DEADLINE_ERROR
+            assert sp.amount == self.data.entry_fee, PAYMENT_ERROR
+            assert not self.data.players.contains(sp.sender), JOIN_ERROR
+            
+            self.data.players[sp.sender] = sp.string("")
 
-    @sp.entry_point
-    def guess(self, content):
-        assert self.data.deadline > sp.now, DEADLINE_ERROR
-        assert self.data.players.contains(sp.sender), GUESS_ERROR
-        assert self.data.players[sp.sender] == sp.string(""), GUESS_TWICE_ERROR
-        assert content != sp.string("")
+        @sp.entry_point
+        def guess(self, content):
+            assert self.data.deadline > sp.now, DEADLINE_ERROR
+            assert self.data.players.contains(sp.sender), GUESS_ERROR
+            assert self.data.players[sp.sender] == sp.string(""), GUESS_TWICE_ERROR
+            assert content != sp.string("")
 
-        self.data.players[sp.sender] = content
-        
+            self.data.players[sp.sender] = content
+
+        @sp.entry_point
+        def reveal(self):
+            pass
+
+@sp.add_test
+def test():
+    alice = sp.test_account("Alice").address
+    bob = sp.test_account("Bob").address
+    eve = sp.test_account("Eve").address
+    sc = sp.test_scenario("Test", main)
+    c1 = main.GuessSeedGame(alice, sp.tez(5), sp.timestamp_from_utc(2025, 12, 31, 23, 59, 59))
+    sc += 1
 
     
         
