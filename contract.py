179870cd044e72f6468bda60239b93d157c32b1c
arthur.lefebvre <arthur.lefebvre@epita.fr>
2025-07-05 14:58:05 +0200

    add a set seed entry point

--- contract.py
+++ contract.py
@@ -9,7 +9,9 @@ DEADLINE_ERROR="You can neither guess nor join a game after the deadline."
 WIN_AMOUNT_ERROR="Only the winner of the game can get the amount of the contract."
 WIN_AMOUNT_DEADLINE_ERROR="The amount of the contract can only be earned AFTER the deadline."
 
+@sp.module
 def main():
+
     class GuessSeedGame(sp.Contract):
         def __init__(self, admin, entry_fee, round_duration, deadline):
             self.data.admin = admin
@@ -17,8 +19,19 @@ def main():
             self.data.round_duration = round_duration
             self.data.players = sp.map()
             self.data.deadline = deadline
-            self.data.winner = sp.address("") # this field is modified OFF-CHAIN
+            self.data.winner = sp.address("") 
+            self.data.seed = sp.string("")
+            self.data.generated_images = sp.map()
         
+
+        @sp.entry_point
+        def set_seed(self, seed):
+            assert sp.sender == self.data.admin, ADMIN_ERROR
+            self.data.seed = seed
+            # call api to set generated_images
+            self.data.players.clear()
+            self.data.winner = sp.address("")
+
         @sp.entry_point
         def join_game(self):
             assert self.data.deadline > sp.now, DEADLINE_ERROR
@@ -38,7 +51,9 @@ def main():
 
         @sp.entry_point
         def reveal(self):
-            pass
+            assert self.data.deadline <= sp.now, REVEAL_DEADLINE_ERROR
+            #call api to set winner
+            
 
         @sp.entry_point
         def win_amount(self):
